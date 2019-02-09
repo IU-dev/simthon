c8202d290ad401db1d683441f6e85dcc77bdcec5
Bondar <24-test@bk.ru>
2019-02-09 17:17:18 +0400

    Tekusheye polozheniye del

--- board.py
+++ board.py
@@ -12,6 +12,9 @@ class Board:
         self._size = size
         self._surface = pygame.Surface([self.get_width(), self.get_height()])
         self._board = [[(0, 0, 0) for i in range(cols)] for i in range(rows)]
+        self._colored = [[(0, 0, 0) for i in range(cols)] for i in range(rows)]
+        self._guessed = [[False for i in range(cols)] for i in range(rows)]
+        self._queue = []
         self._tracer = tracer.Tracer(self._board)
         self._active = False
         self._colors = [
@@ -20,6 +23,32 @@ class Board:
             (0, 0, 255),
             (255, 255, 0)
         ]
+        for i in range(len(self._colored)):
+            for j in range(len(self._colored[i])):
+                self._colored[i][j] = self._colors[random.randint(0, 3)]
+        self._ticks = 0
+
+    def guess(self):
+        row = random.randint(0, 9)
+        cell = random.randint(0, 9)
+        self._guessed[row][cell] = True
+        self._queue.append((row, cell))
+        print(self._queue)
+
+    def show_combination(self):
+        for cell in self._queue:
+            if self._guessed[cell[0]][cell[1]]:
+                self._board[cell[0]][cell[1]] = self._colored[cell[0]][cell[1]]
+                self._ticks = 1000
+
+    def handle_timer(self, ticks):
+        if self._ticks > 0:
+            self._ticks -= ticks
+            if self._ticks <= 0:
+                self._ticks = 0
+                for cell in self._queue:
+                    if self._guessed[cell[0]][cell[1]]:
+                        self._board[cell[0]][cell[1]] = (0, 0, 0)
 
     def get_surface(self):
         return self._surface
@@ -47,7 +76,6 @@ class Board:
             pygame.draw.rect(self._surface, (255, 255, 255), (my_left, my_top, self._size, self._size), 1)
 
     def get_cell(self, x, y):
-        print(x // self._size, y // self._size)
         return x // self._size, y // self._size
 
     def get_item(self, cell):
@@ -65,10 +93,9 @@ class Board:
     def click(self, pos):
         foo = self.get_cell(pos[0], pos[1])
         if foo is None:
-            print('None')
+            return None
         else:
             self.clicked_cell = foo
-            print(self.clicked_cell)
 
     def step(self):
         if self._active:
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ screen = pygame.display.set_mode(size)
 pygame.init()
 board = board.Board(10, 10, 50)
 # board.generate_random(3)
+clock = pygame.time.Clock()
 
 while running:
     screen.fill((0, 0, 0))
@@ -14,14 +15,18 @@ while running:
     screen.blit(board_surface, (50, 50))
     pygame.display.flip()
 
-    event = pygame.event.wait()
-    if event.type == pygame.QUIT:
-        running = False
-    elif event.type == pygame.MOUSEBUTTONDOWN:
-        board_rect = board_surface.get_rect().move(50,50)
-        if board_rect.collidepoint(event.pos):
-            board_pos = event.pos[0] - 50, event.pos[1] - 50
-            board.click(board_pos)
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            board_rect = board_surface.get_rect().move(50,50)
+            if board_rect.collidepoint(event.pos):
+                board_pos = event.pos[0] - 50, event.pos[1] - 50
+                board.click(board_pos)
+            board.guess()
+            board.show_combination()
     pygame.display.flip()
+    board.handle_timer(clock.tick())
 
 pygame.quit()
